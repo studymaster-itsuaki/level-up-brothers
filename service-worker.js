9d0aec042115c823a52046e1e61c406b937354f9
rarurin99 <61084487+rarurin99@users.noreply.github.com>
2026-08-04 22:56:21 +0900

    level-up-brothers-v1.0.zip

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "lub-beta8-pwa-fcm-v3";
+const CACHE_VERSION = "lub-beta8-pwa-fcm-v4";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-app-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
