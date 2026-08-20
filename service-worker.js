5faf1cbc37684ba36b1a2535fefba888ce453ea3
rarurin99 <61084487+rarurin99@users.noreply.github.com>
2026-08-21 08:33:29 +0900

    母履歴修正版

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "lub-beta8-pwa-fcm-v13";
+const CACHE_VERSION = "lub-beta8-pwa-fcm-v14";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-app-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
