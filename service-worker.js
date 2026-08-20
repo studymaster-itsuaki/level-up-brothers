b9358a6e5b23b85720ca88679b36518e313e87c6
rarurin99 <61084487+rarurin99@users.noreply.github.com>
2026-08-21 08:50:22 +0900

    余計な情報削除ver

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "lub-beta8-pwa-fcm-v14";
+const CACHE_VERSION = "lub-beta8-pwa-fcm-v15";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-app-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
